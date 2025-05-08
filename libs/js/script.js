be0f31b65a0be746fc5a22b5ddce221777f34595
kueneargomido <kueneargomido@gmail.com>
2025-05-08 17:59:09 +0800

    Update script.js

--- libs/js/script.js
+++ libs/js/script.js
@@ -129,14 +129,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Form submission (example)
-    const contactForm = document.querySelector('.contact-form form');
-    if (contactForm) {
-        contactForm.addEventListener('submit', function(e) {
-            e.preventDefault();
-            alert('Thank you for your message! I will get back to you soon.');
-            this.reset();
-        });
-    }
+    
 
     // Newsletter form submission (example)
     const newsletterForm = document.querySelector('.footer-newsletter form');
